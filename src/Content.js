48458d0ddfe0cf2f370a750118a430894c45aaf1
azalia <mahsa.abdi2@gmail.com>
2023-07-02 11:48:42 +0330

    create new icons

--- src/Content.js
+++ src/Content.js
@@ -5,7 +5,7 @@ const Content = () =>{
     const[items, setItem]=useState([
       {
         id: 1,
-        checked: false,
+        checked: true,
         item: "one half"
       },
       {
@@ -20,24 +20,41 @@ const Content = () =>{
       },
     ])
     
-    
+    const handlecHECK =(id) =>{
+      // console.log(`key: ${id}`)
+      const listItems=items.map((item)=> item.id === id ? { ...item, checked: !item.checked } :item);
+      setItem(listItems)
+      localStorage.setItem('shoppinglist' , JSON.stringify(listItems));
+    }
 
-    
+    const handleDelete= (id) =>{
+      // console.log(id)
+      const listItems=items.filter((item) => item.id !== id);
+      setItem(listItems)
+      localStorage.setItem('shoppinglist' , JSON.stringify(listItems));
+    }
 
    
 
     return(
       <main>
+        {items.length ? (
         <ul>
           {items.map((item)=>(
             <li className="item" key={item.id}>
-                <input type="checkbox" checked={item.checked} />
-                <label>{item.item}</label>
-                <FaTrashAlt role="button" tabIndex="0" />
+                <input type="checkbox" onChange={()=> handlecHECK(item.id)} checked={item.checked} />
+                <label
+                  onDoubleClick={()=> handlecHECK(item.id)}
+                  style={(item.checked) ? {textDecoration: 'line-through'}: null}
+                >{item.item}</label>
+                <FaTrashAlt onClick={()=>handleDelete(item.id)} role="button" tabIndex="0" />
 
             </li>
           ))}
         </ul>
+      ):(
+          <p style={{marginTop: '2rem'}}>your list is</p>
+        )}
       </main>
         
     )
